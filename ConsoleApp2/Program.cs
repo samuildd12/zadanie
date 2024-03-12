cf64441d39eaf18077c03740b406d392930ad4ea
ioweaj <pefos43362@azduan.com>
2024-03-12 13:55:14 +0200

    Zadanie upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 1;
+            int tempCount = 1;
+            int number = 0;
+
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] == arr[i + 1])
+                {
+                    tempCount++;
+                }
+                else
+                {
+                    tempCount = 1;
+                }
+
+                if (tempCount > count)
+                {
+                    count = tempCount;
+                    number = arr[i];
+                }
+                
+            }
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write("{0}, ", number);
+                Thread.Sleep(1500);
+            }
+        }
+    }
+}
